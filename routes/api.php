ff87d9cecd6613c6af30dc69ee168a4b57712541
Coding Geek <web.teamlead.imh.it@gmail.com>
2024-10-24 11:22:40 +0630

    Refactor : Modify Controller for Api  with Suffixe ApiController

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\ProductController;
-use App\Http\Controllers\Api\CategoryController;
-use App\Http\Controllers\Api\OrderController;
-use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\ProductApiController;
+use App\Http\Controllers\Api\CategoryApiController;
+use App\Http\Controllers\Api\OrderApiController;
+use App\Http\Controllers\Api\UserApiController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -15,11 +15,11 @@ Route::get('/test', function () {
     return response()->json(['message' => 'API is working!']);
 });
 
-Route::middleware('auth:sanctum')->group( function () {
-    Route::resource('products', ProductController::class);
-});
+// Route::middleware('auth:sanctum')->group( function () {
+//     Route::resource('products', ProductApiController::class);
+// });
 
-Route::apiResource('products', ProductController::class);
-Route::apiResource('categories', CategoryController::class);
-Route::apiResource('orders', OrderController::class);
-Route::apiResource('users', UserController::class);
+Route::apiResource('products', ProductApiController::class);
+Route::apiResource('categories', CategoryApiController::class);
+Route::apiResource('orders', OrderApiController::class);
+Route::apiResource('users', UserApiController::class);
