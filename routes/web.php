20acb0c93378d0c6322cc01d06751771d9afebba
Coding Geek <web.teamlead.imh.it@gmail.com>
2024-10-24 15:57:35 +0630

    Add Auth for Web with Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Web\UserController;
 use App\Http\Controllers\Web\ProductController;
@@ -7,20 +8,39 @@ use App\Http\Controllers\Web\CategoryController;
 use App\Http\Controllers\Web\OrderController;
 use App\Http\Controllers\Web\OrderItemController;
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // User Routes
+    Route::resource('users', UserController::class);
+
+    // Category Routes
+    Route::resource('categories', CategoryController::class);
+
+    // Product Routes
+    Route::resource('products', ProductController::class);
+
+    // Order Routes
+    Route::resource('orders', OrderController::class);
+
+    // Order Item Routes
+    Route::resource('order-items', OrderItemController::class);
+
+});
+
 // Change welcome page to product list
-Route::get('/', [ProductController::class, 'index']);
+// Route::get('/', [ProductController::class, 'index']);
 
-// User Routes
-Route::resource('users', UserController::class);
 
-// Category Routes
-Route::resource('categories', CategoryController::class);
 
-// Product Routes
-Route::resource('products', ProductController::class);
-
-// Order Routes
-Route::resource('orders', OrderController::class);
-
-// Order Item Routes
-Route::resource('order-items', OrderItemController::class);
+require __DIR__.'/auth.php';
